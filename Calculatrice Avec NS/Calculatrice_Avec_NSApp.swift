4fbd86ad6dacba4fe2b0352b8162acdbcc2bd7b8
Bilal <yyybilal@gmail.com>
2021-12-13 13:59:16 +0100

    Initial Commit

---
+++ Calculatrice Avec NS/Calculatrice_Avec_NSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculatrice_Avec_NSApp.swift
+//  Calculatrice Avec NS
+//
+//  Created by bilal on 13/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Calculatrice_Avec_NSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
